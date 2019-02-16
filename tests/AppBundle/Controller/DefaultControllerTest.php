4ace2bf8e3875990a1699b232fc76b775889e13e
Amelie DUVERNET <amelieonline@gmail.com>
2019-02-16 16:15:25 +0100

    Installing phpunit

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -4,15 +4,12 @@ namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class DemoControllerTest extends WebTestCase
 {
     public function testIndex()
     {
         $client = static::createClient();
-
-        $crawler = $client->request('GET', '/');
-
+        $client->request('GET', '/');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
 }
