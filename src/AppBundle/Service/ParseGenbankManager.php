a84d9a007bd784a20fb1c7ac581179b6fa955bc5
Amelie DUVERNET <amelieonline@gmail.com>
2019-02-20 16:06:09 +0100

    Refactoring ParseGenbank (in progress)

--- src/AppBundle/Service/ParseGenbankManager.php
+++ src/AppBundle/Service/ParseGenbankManager.php
@@ -12,39 +12,25 @@ use AppBundle\Entity\Sequence;
 
 class ParseGenbankManager
 {
-    private $seqarr;
-    private $inseq_flag;
-    private $seqdata_flag;
-    private $accession_flag;
     private $ref_array;
-    private $feature_array;
-    private $entry_ctr;
-    private $ref_ctr;
     private $maxlength;
     private $minlength;
     private $tot_seqlength;
     private $seqdata;
     private $aLines;
 
+
     /**
      * ParseGenbankManager constructor.
      */
     public function __construct()
     {
-        $this->seqarr         = [];
-        $this->inseq_flag     = false;
-        $this->seqdata_flag   = false;
-        $this->accession_flag = false;
         $this->ref_array      = [];
-        $this->feature_array  = [];
-        $this->entry_ctr      = 0;
-        $this->ref_ctr        = 0;
         $this->maxlength      = 0;
         $this->minlength      = 999999;
         $this->tot_seqlength  = 0;
         $this->seqdata        = "";
     }
-
 
 
     /**
@@ -68,35 +54,51 @@ class ParseGenbankManager
 
             switch(trim(substr($this->aLines->current(),0,12))) {
                 case "LOCUS":
-                    if($oSequence->getId() == null) {
-                        $this->parseLocus($oSequence);
-                    }
+                    $this->parseLocus($oSequence);
                     break;
                 case "DEFINITION":
-                    if($oSequence->getDefinition() == null) {
-                        $this->parseDefinition($oSequence);
-                    }
+                    $this->parseDefinition($oSequence, $flines);
                     break;
                 case "ORGANISM":
                     $oSequence->setOrganism(trim(substr($linestr,12)));
                     break;
                 case "VERSION":
-                    if($oSequence->getVersion() == null) {
-                        $this->parseVersion($oSequence);
-                    }
+                    $this->parseVersion($oSequence);
                     break;
                 case "KEYWORDS":
-                    if($oSequence->getKeywords() == null) {
-                        $this->parseKeywords($oSequence);
-                    }
+                    $this->parseKeywords($oSequence);
                     break;
                 case "ACCESSION":
-                    if($oSequence->getAccession() == null) {
-                        $this->parseAccession($oSequence);
-                    }
+                    $this->parseAccession($oSequence);
                     break;
                 case "FEATURES":
-
+                    $aFeatures = array();
+                    while(1) {
+                        // Verify next line
+                        $head = trim(substr($flines[$this->aLines->key()+1],0, 20));
+                        if($head != "" && ($head !="source" && $head !="gene" && $head !="exon" && $head !="CDS" && $head !="misc_feature")) {
+                            break;
+                        }
+                        $this->aLines->next();
+                        switch (trim(substr($this->aLines->current(), 0, 20))) {
+                            case "source":
+                                $this->parseFeatures($aFeatures, $flines, "source");
+                                break;
+                            case "gene":
+                                $this->parseFeatures($aFeatures, $flines, "gene");
+                                break;
+                            case "exon":
+                                $this->parseFeatures($aFeatures, $flines, "exon");
+                                break;
+                            case "CDS":
+                                $this->parseFeatures($aFeatures, $flines, "CDS");
+                                break;
+                            case "misc_feature":
+                                $this->parseFeatures($aFeatures, $flines, "misc_feature");
+                                break;
+                        }
+                    }
+                    $oSequence->setFeatures($aFeatures);
                     break;
                 case "REFERENCE":
                     $wordarray = preg_split("/\s+/", trim(substr($this->aLines->current(),12)));
@@ -191,7 +193,7 @@ class ParseGenbankManager
     private function seekReferences(&$sReferenceProperty)
     {
         while(1) {
-            $referencePropertyLine = preg_split("/\s+/", trim(substr($this->aLines->current(), 12))); // AUTHORS
+            $referencePropertyLine = preg_split("/\s+/", trim(substr($this->aLines->current(), 12)));
             $sReferenceProperty = trim($sReferenceProperty." ".implode(" ", $referencePropertyLine));
             $this->aLines->next();
             $head = trim(substr($this->aLines->current(), 0, 12));
@@ -201,174 +203,6 @@ class ParseGenbankManager
         }
         return $sReferenceProperty;
     }
-
-    /*public function parse_id($flines)
-    {
-        $oSequence = new Sequence();
-        $feat_r = [];
-
-        foreach($flines as $lineno => $linestr) {
-
-
-            if (trim(substr($linestr,0,10)) == "BASE COUNT") {
-                $oSequence = $this->parseBaseCount($feat_r, $oSequence);
-            }
-
-            if (trim(substr($linestr,0,12)) == "FEATURES") {
-                // The REFERENCE section was present for this SEQUENCE ENTRY so we set REFERENCE attribute.
-                if (count($this->ref_array) > 0) {
-                    $oSequence->setReference($this->ref_array);
-                }
-                $lastsubkey = "";
-
-                $feat_r = [];
-                $qual_r = [];
-
-                // Go to the next line.
-                list($lineno, $linestr) = each($flines);
-
-
-                // This loops through each line in the entire FEATURES SECTION.
-                while(substr($linestr,0,10) != "BASE COUNT") {
-                    $label = trim(substr($linestr,0,21));
-                    $data = trim(substr($linestr,21));
-
-                    if (strlen($label) != 0) {
-                        // At the beginning of a new SUBKEY.
-                        $subkey = $label;
-                        // Add/save the qualifier array (qual_r) of the previous SUBKEY to our big feat_r array.
-                        if (count($qual_r) > 0) {
-                            $feat_r[$lastsubkey] = $qual_r;
-                            $qual_r = array();
-                        }
-
-                        $qual = $subkey;
-                        $qual_r[$qual] = "";
-                        $qual_ctr = 0;
-                        dump($oSequence);exit();
-                        do {
-                            $qual_ctr++;
-                            $qual_r[$qual] .= " " . $data;
-
-                            //list($lineno, $linestr) = each($flines);
-                            $label = trim(substr($linestr,0,21));
-                            $data = trim(substr($linestr,21));
-                        } while ($label == "" && !($this->isa_qualifier($data)));
-
-                        if (!($label == "")) {
-                            $lastsubkey = $subkey;
-                            $subkey = $label;
-                        }
-                    } else { // we are inside a subkey section but on the 2nd, 3rd, nth line which have blank LABELS.
-                        if ($this->isa_qualifier($data)) {
-                            $wordarray = preg_split("/=/", $data);
-                            $qual = $wordarray[0];
-                            $data = $wordarray[1];
-                            $qual_r[$qual] = "";
-                            $qual_ctr = 0;
-
-                            do {
-                                $qual_ctr++;
-                                $qual_r[$qual] .= " " . $data;
-                                //list($lineno, $linestr) = each($flines);
-                                $label = trim(substr($linestr,0,21));
-                                $data = trim(substr($linestr,21));
-                            } while($label == "" && !($this->isa_qualifier($data)));
-
-                            if (!($label == "")) {
-                                $lastsubkey = $subkey;
-                                $subkey = $label;
-                            }
-                        }
-                    }
-                }
-
-                if (count($qual_r) > 0) {
-                    $feat_r[$lastsubkey] = $qual_r;
-                    $qual_r = [];
-                }
-
-                prev($flines);
-            }
-
-
-
-            if ($this->inseq_flag) {
-                if (trim(substr($linestr, 0, 12)) == "REFERENCE") {
-                    // at this point, we are at the line with REFERENCE x (base y of z) in it.
-                    $wordarray = preg_split("/\s+/", trim(substr($linestr,12)));
-
-                    $ref_rec = array();
-                    $ref_rec["REFNO"] = $wordarray[0];
-                    array_shift($wordarray);
-                    $ref_rec["BASERANGE"] = implode(" ", $wordarray);
-                    $lastsubkey = "";
-                    $subkey_lnctr = 0;
-
-                    while(list($lineno, $linestr) = each($flines)) {
-                        $subkey = trim(substr($linestr,0,12));
-
-                        // If current subkey is blank string, then this is a continuation of the last subsection.
-                        if (strlen($subkey) == 0) {
-                            $subkey = $lastsubkey;
-                        }
-
-                        // If we are at the next subkey section (e.g. lastsubkey was AUTHORS, and current is TITLE).
-                        if ($subkey != $lastsubkey) {
-                            $subkey_lnctr = 0;
-                        }
-
-
-                        if ($subkey == "FEATURES") {
-                            prev($flines);
-                            break;
-                        }
-                        if ($subkey == "REFERENCE") {
-                            $this->ref_ctr++;
-                            prev($flines);
-                            break;
-                        }
-                        $lastsubkey = $subkey;
-                    }
-                    array_push($this->ref_array, $ref_rec);
-                }
-
-                if (trim(substr($linestr, 0, 12)) == "SEGMENT") {
-                    $oSequence->setSegment(substr($linestr, 12));
-                    $wordarray = preg_split("/\s+/", trim(substr($linestr,12)));
-                    $oSequence->setSegmentNo($wordarray[0]);
-                    $oSequence->setSegmentCount($wordarray[2]);
-                }
-
-                if ($this->accession_flag) {
-                    // 2nd, 3rd, etc. line of ACCESSION field.
-                    $wordarray = preg_split("/\s+/", trim($linestr));
-                    //$this->sec_accession = array_merge($this->sec_accession, $wordarray); -> mistake ?
-                    $oSequence->setSecAccession(array_merge($oSequence->getSecAccession(), $wordarray));
-                }
-
-                if (($this->seqdata_flag == true) && (substr($linestr,0,2) != "//")) {
-                    $wordarray = explode(" ", trim($linestr));
-                    array_shift($wordarray);
-                    $seqline = implode("", $wordarray);
-                    $this->seqdata .= $seqline;
-                }
-                if (substr($linestr,0,6) == "ORIGIN") {
-                    $this->seqdata_flag = true;
-                }
-                if (substr($linestr,0,2) == "//") {
-                    $oSequence->setSequence($this->seqdata);
-                    $this->seqarr[$this->database->getId()] = $this;
-                    $this->seqdata_flag = false;
-                    $this->inseq_flag = false;
-                    break;
-                }
-            }
-
-        }
-        $oSequence->setSequence($this->seqarr);
-        return $oSequence;
-    }*/
 
 
     /**
@@ -409,32 +243,37 @@ class ParseGenbankManager
         $oSequence->setDivision(strtoupper(trim(substr($this->aLines->current(), 64, 3))));
         $oSequence->setDate(strtoupper(trim(substr($this->aLines->current(), 68, 11))));
 
-        $this->inseq_flag = true;
+        //$this->inseq_flag = true;
         
         return $oSequence;
     }
 
+
     /**
-     * Parses line DEFINITION
+     * Parses DEFINITION field
      * @param $oSequence
      */
-    private function parseDefinition(&$oSequence)
+    private function parseDefinition(&$oSequence, $flines)
     {
         $wordarray = explode(" ", $this->aLines->current());
         array_shift($wordarray);
         $sDefinition = trim(implode(" ", $wordarray));
         while(1) {
-            $this->aLines->next();
-            $head = trim(substr($this->aLines->current(),0,12));
+            $head = trim(substr($flines[$this->aLines->key()+1],0, 12));
             if($head != "") {
-                $this->aLines->rewind();
                 break;
             }
+            $this->aLines->next();
             $sDefinition .= " ".trim($this->aLines->current());
         }
         $oSequence->setDefinition($sDefinition);
     }
 
+
+    /**
+     * Parses VERSION field
+     * @param $oSequence
+     */
     private function parseVersion(&$oSequence)
     {
         $wordarray = preg_split("/\s+/", trim($this->aLines->current()));
@@ -444,6 +283,11 @@ class ParseGenbankManager
         }
     }
 
+
+    /**
+     * Parses KEYWORDS field
+     * @param $oSequence
+     */
     private function parseKeywords(&$oSequence)
     {
         $wordarray = preg_split("/\s+/", trim($this->aLines->current()));
@@ -454,6 +298,11 @@ class ParseGenbankManager
         }
     }
 
+
+    /**
+     * Parses ACCESSION field
+     * @param object $oSequence
+     */
     private function parseAccession(&$oSequence)
     {
         $wordarray = preg_split("/\s+/", trim($this->aLines->current()));
@@ -461,5 +310,38 @@ class ParseGenbankManager
         array_shift($wordarray);
         array_shift($wordarray);
         $oSequence->setSecAccession($wordarray);
+    }
+
+
+    /**
+     * Parses each fields for FEATURES
+     * @param   array   $aFeatures
+     * @param   array   $aFlines
+     * @param   string  $sField
+     */
+    private function parseFeatures(&$aFeatures, $aFlines, $sField)
+    {
+        $sKey = $sField ." ". implode(" ", preg_split("/\s+/", trim(substr($this->aLines->current(), 20))));
+        $aMyFeature = array();
+        $this->aLines->next();
+        $sLine = implode(" ", preg_split("/\s+/", trim(substr($this->aLines->current(), 20))));
+        while (1) {
+            // If line begins with  /
+            // Adding line in array
+            if(substr(trim($aFlines[$this->aLines->key()+1]), 0, 1) == "/") {
+                $aMyFeature[] = $sLine;
+                $sLine = ""; // RAZ
+            }
+
+            $this->aLines->next();
+            $sLine .= " ".implode(" ", preg_split("/\s+/", trim(substr($this->aLines->current(), 20))));
+
+            $sHead = trim(substr($aFlines[$this->aLines->key()+1],0, 12));
+            if($sHead != "") { // Stop if we change feature
+                $aMyFeature[] = $sLine;
+                break;
+            }
+        }
+        $aFeatures[$sKey] = $aMyFeature;
     }
 }
