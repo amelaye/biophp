48d69b9503ba67ae87985c6a2be7d83c4626a177
Amelie DUVERNET <amelieonline@gmail.com>
2019-03-31 17:26:46 +0200

    Microsatellite repeats finder OK + Refacto

--- src/MinitoolsBundle/Service/MicrosatelliteRepeatsFinderManager.php
+++ src/MinitoolsBundle/Service/MicrosatelliteRepeatsFinderManager.php
@@ -19,59 +19,59 @@ class MicrosatelliteRepeatsFinderManager
      * which shows a repeated pattern, as for example in sequence 'ACGTACGTACGTACGT', where 'ACGT' is repeated
      * 4 times. The function allows searching for this kind of subsequences within a sequence.
      * so that sequence AACCGGTT-AAGCGGTT-AACCGGAT-AACCGGTT may be considered as a microsatellite repeat
-     * @param   string      $sequence               is the sequence
-     * @param   int         $min_length             are the range of oligo lengths to be searched; p.e. oligos with length 2 to 6
-     * @param   int         $max_length             are the range of oligo lengths to be searched; p.e. oligos with length 2 to 6
-     * @param   int         $min_repeats            minimal number of time a sequence must be repeated to be considered as a microsatellite repeat
-     * @param   int         $min_length_of_MR       minimum length of tandem repeat; to avoid considering AAAA as a microsatellite repeat, set it to >4
-     * @param   int         $mismatches_allowed     the porcentage of errors allowed when searching in the repetitive sequence
+     * @param   string      $sSequence              is the sequence
+     * @param   int         $iMinLength             are the range of oligo lengths to be searched; p.e. oligos with length 2 to 6
+     * @param   int         $iMaxLength             are the range of oligo lengths to be searched; p.e. oligos with length 2 to 6
+     * @param   int         $iMinRepeats            minimal number of time a sequence must be repeated to be considered as a microsatellite repeat
+     * @param   int         $iMinLengthOMR          minimum length of tandem repeat; to avoid considering AAAA as a microsatellite repeat, set it to >4
+     * @param   int         $iMismatchesAllowed     the porcentage of errors allowed when searching in the repetitive sequence
      * @return  array
      * @throws  \Exception
      */
-    public function findMicrosatelliteRepeats($sequence, $min_length, $max_length, $min_repeats, $min_length_of_MR, $mismatches_allowed)
+    public function findMicrosatelliteRepeats($sSequence, $iMinLength, $iMaxLength, $iMinRepeats, $iMinLengthOMR, $iMismatchesAllowed)
     {
         try {
-            $len_seq = strlen($sequence);
-            $counter = 0;
-            for ($i = 0; $i < $len_seq-3; $i++) {
-                for($j = $min_length; $j < $max_length+1; $j++) {
-                    if(($i+$j) > $len_seq) {
+            $iLenSeq = strlen($sSequence);
+            $iCounter = 0;
+            $aResults = [];
+
+            for ($i = 0; $i < $iLenSeq-3; $i++) {
+                for($j = $iMinLength; $j < $iMaxLength+1; $j++) {
+                    if(($i+$j) > $iLenSeq) {
                         break;
                     }
-                    $sub_seq = substr($sequence,$i,$j);
-                    $len_sub_seq = strlen($sub_seq);
-                    $mismatches = floor($len_sub_seq * $mismatches_allowed / 100);
+                    $sSubSeq = substr($sSequence,$i,$j);
+                    $iLenSubSeq = strlen($sSubSeq);
+                    $mismatches = floor($iLenSubSeq * $iMismatchesAllowed / 100);
 
                     switch($mismatches) {
                         case 1:
-                            $sub_seq_pattern = $this->includeN1($sub_seq,0);
+                            $sSubSeqPattern = $this->includeN1($sSubSeq,0);
                             break;
                         case 2:
-                            $sub_seq_pattern = $this->includeN2($sub_seq,0);
-                            break;
                         case 3:
-                            $sub_seq_pattern = $this->includeN3($sub_seq,0);
+                            $sSubSeqPattern = $this->includeNPlus1($sSubSeq,0);
                             break;
                         default:
-                            $sub_seq_pattern = $sub_seq;
+                            $sSubSeqPattern = $sSubSeq;
                     }
 
-                    $matches = 1;
-                    while(preg_match_all("/($sub_seq_pattern)/",substr($sequence,($i+$j*$matches),$j),$out) == 1) {
-                        $matches++;
+                    $iMatches = 1;
+                    while(preg_match_all("/($sSubSeqPattern)/",substr($sSequence,($i+$j*$iMatches),$j),$out) == 1) {
+                        $iMatches ++;
                     }
 
-                    if($matches >= $min_repeats && ($j*$matches) >= $min_length_of_MR) {
-                        $results[$counter]["start_position"] = $i;
-                        $results[$counter]["length"] = $j;
-                        $results[$counter]["repeats"] = $matches;
-                        $results[$counter]["sequence"] = substr($sequence,$i,$j*$matches);
-                        $counter ++;
-                        $i += $j * $matches;
+                    if($iMatches >= $iMinRepeats && ($j * $iMatches) >= $iMinLengthOMR) {
+                        $aResults[$iCounter]["start_position"] = $i;
+                        $aResults[$iCounter]["length"] = $j;
+                        $aResults[$iCounter]["repeats"] = $iMatches;
+                        $aResults[$iCounter]["sequence"] = substr($sSequence, $i,$j * $iMatches);
+                        $iCounter ++;
+                        $i += $j * $iMatches;
                     }
                 }
             }
-            return($results);
+            return($aResults);
         } catch (\Exception $e) {
             throw new \Exception($e);
         }
@@ -85,21 +85,21 @@ class MicrosatelliteRepeatsFinderManager
      * is a numeric value which indicates number of bases always maching  the DNA sequence in 3' end.
      * For example, when $minus is 1, the pattern for "acgt" will be  ".cgt|a.gt|ac.t".
      * Check also IncludeN2 and IncludeN3.
-     * @param   string      $primer     DNA sequence (oligonucleotide, primer)
-     * @param   int         $minus      indicates number of bases in 3' which will always much the DNA sequence.
-     * @return  string                  pattern
+     * @param   string      $sPrimer     DNA sequence (oligonucleotide, primer)
+     * @param   int         $iMinus      indicates number of bases in 3' which will always much the DNA sequence.
+     * @return  string                   pattern
      * @throws \Exception
      */
-    public function includeN1($primer, $minus)
+    public function includeN1($sPrimer, $iMinus)
     {
         try {
-            $code = ".".substr($primer,1);
-            $wpos = 1;
-            while($wpos < strlen($primer) - $minus) {
-                $code .= "|".substr($primer,0,$wpos).".".substr($primer,$wpos+1);
-                $wpos ++;
+            $sCode = ".".substr($sPrimer,1);
+            $iWpos = 1;
+            while($iWpos < strlen($sPrimer) - $iMinus) {
+                $sCode .= "|".substr($sPrimer,0,$iWpos).".".substr($sPrimer,$iWpos + 1);
+                $iWpos ++;
             }
-            return $code;
+            return $sCode;
         } catch (\Exception $e) {
             throw new \Exception($e);
         }
@@ -113,55 +113,26 @@ class MicrosatelliteRepeatsFinderManager
      * is a numeric value which indicates number of bases always maching  the DNA sequence in 3' end.
      * For example, when $minus is 1, the pattern for "acgt" will be  "..gt|.c.t|a..t".
      * Check also IncludeN1 and IncludeN3.
-     * @param   string      $primer     DNA sequence (oligonucleotide, primer)
-     * @param   string      $minus      number of bases in 3' which will always much the DNA sequence.
-     * @return  string
+     * @param   string      $sPrimer     DNA sequence (oligonucleotide, primer)
+     * @param   string      $iMinus      Number of bases in 3' which will always much the DNA sequence.
+     * @return  string                   Pattern
      * @throws  \Exception
      */
-    public function includeN2($primer, $minus)
+    public function includeNPlus1($sPrimer, $iMinus)
     {
         try {
-            $max = strlen($primer) - $minus;
-            $code = "";
-            for($i = 0; $i < $max; $i++) {
-                for($j=0; $j < $max-$i-1; $j++) {
-                    $code .= "|".substr($primer,0,$i).".";
-                    $resto = substr($primer,$i+1);
-                    $code .= substr($resto,0,$j).".".substr($resto,$j+1);
+            $iMax = strlen($sPrimer) - $iMinus;
+            $sCode = "";
+            for($i = 0; $i < $iMax; $i++) {
+                for($j = 0; $j < $iMax - $i - 1; $j++) {
+                    $sCode .= "|".substr($sPrimer,0,$i).".";
+                    $sResto = substr($sPrimer,$i+1);
+                    $sCode .= substr($sResto,0,$j).".".substr($sResto,$j+1);
                 }
             }
 
-            $code = substr($code,1);
-            return $code;
-        } catch (\Exception $e) {
-            throw new \Exception($e);
-        }
-    }
-
-
-    /**
-     *
-     * Similar to function IncludeN1 and IncludeN2, but allows two missmaches. The parameter $minus
-     * is a numeric value which indicates number of bases always maching  the DNA sequence in 3' end.
-     * @param   string     $primer     DNA sequence (oligonucleotide, primer)
-     * @param   int        $minus      indicates number of bases in 3' which will always much the DNA sequence.
-     * @return  string
-     * @throws  \Exception
-     */
-    public function includeN3($primer, $minus)
-    {
-        try {
-            $max = strlen($primer) - $minus;
-            $code = "";
-            for($i = 0; $i < $max; $i++) {
-                for($j = 0; $j < $max-$i-1; $j++) {
-                    $code.="|".substr($primer,0,$i).".";
-                    $resto = substr($primer,$i+1);
-                    $code .= substr($resto,0,$j).".".substr($resto,$j+1);
-                }
-            }
-            $code = substr($code,1);
-            return $code;
+            $sCode = substr($sCode,1);
+            return $sCode;
         } catch (\Exception $e) {
             throw new \Exception($e);
         }
