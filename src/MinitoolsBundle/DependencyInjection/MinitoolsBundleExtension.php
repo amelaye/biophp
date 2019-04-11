8e8c94b154d883f9374aa26f136854bbd0f343b9
Amelie DUVERNET <amelieonline@gmail.com>
2019-04-11 14:26:00 +0200

    Moving services into bundles

---
+++ src/MinitoolsBundle/DependencyInjection/MinitoolsBundleExtension.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amelaye
+ * Date: 2019-04-11
+ * Time: 00:12
+ */
+
+namespace MinitoolsBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+use Sensio\Bundle\FrameworkExtraBundle\DependencyInjection\Configuration;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+/**
+ * Class AppBundleExtension
+ * @package AppBundle\DependencyInjection
+ */
+class MinitoolsBundleExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+
+        $loader->load('services.yml');
+    }
+}
