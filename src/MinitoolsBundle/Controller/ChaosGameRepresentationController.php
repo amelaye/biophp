c9ebe8d6ff0886da83450c307fde8c7ec9bbf206
Amelie DUVERNET <amelieonline@gmail.com>
2019-07-22 19:24:09 +0200

    Ordering controllers

--- src/MinitoolsBundle/Controller/ChaosGameRepresentationController.php
+++ src/MinitoolsBundle/Controller/ChaosGameRepresentationController.php
@@ -7,15 +7,14 @@
  */
 namespace MinitoolsBundle\Controller;
 
-
-use AppBundle\Bioapi\Bioapi;
-use AppBundle\Service\OligosManager;
-
-use AppBundle\Traits\OligoTrait;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+use AppBundle\Bioapi\Bioapi;
+use AppBundle\Service\OligosManager;
+use AppBundle\Traits\OligoTrait;
 
 use MinitoolsBundle\Form\ChaosGameRepresentationType;
 use MinitoolsBundle\Service\ChaosGameRepresentationManager;
