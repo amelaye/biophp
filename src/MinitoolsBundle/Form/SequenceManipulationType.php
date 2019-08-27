25686a946c211743d1b50b7ab1df73dc93e61cc6
Amélie DUVERNET <amelieonline@gmail.com>
2019-08-27 21:02:26 +0200

    Adding forms for Sequences manipulations

--- src/MinitoolsBundle/Form/SequenceManipulationType.php
+++ src/MinitoolsBundle/Form/SequenceManipulationType.php
@@ -3,20 +3,17 @@
  * SequenceManipulation Form Inputs
  * Freely inspired by BioPHP's project biophp.org
  * Created 22 july 2019
- * Last modified 22 july 2019
+ * Last modified 27 august 2019
  */
 namespace MinitoolsBundle\Form;
 
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\FormEvents;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Validator\Context\ExecutionContextInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Callback;
 
 /**
  * Class SequenceManipulationType
@@ -32,12 +29,20 @@ class SequenceManipulationType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $data = "GGAGTGAGGGGAGCAGTTGGGCCAAGATGGCGGCCGCCGAGGGACCGGTGGGCGACGCGG\r
-        GAGTGAGGGGAGCAGTTGGGCCAAGATGGCGGCCGCCGAGGGACCGGTGGGCGACGGGGG\r
-        AGTGAGGGGAGCAGTTGGGCCAAGATGGCGGCCGCCGAGGGACCGGTGGGCGACGGCGGA\r
-        GTGAGGGGAGCAGTTGGGCCAAGATGGCGGCCGCCGAGGGACCGGTGGGCGACGGGGAGT\r
-        GAGGGGAGCAGTTGGGCCAAGATGGCGGCCGCCGAGGGACCGGTGGGCGACGCGGGAGTG\r";
+        $data = "GGAGTGAGGGGAGCAGTTGGGCCAAGATGGCGGCCGCCGAGGGACCGGTGGGCGACGCGG\r";
+        $data.= "GAGTGAGGGGAGCAGTTGGGCCAAGATGGCGGCCGCCGAGGGACCGGTGGGCGACGGGGG\r";
+        $data.= "AGTGAGGGGAGCAGTTGGGCCAAGATGGCGGCCGCCGAGGGACCGGTGGGCGACGGCGGA\r";
+        $data.= "GTGAGGGGAGCAGTTGGGCCAAGATGGCGGCCGCCGAGGGACCGGTGGGCGACGGGGAGT\r";
+        $data.= "GAGGGGAGCAGTTGGGCCAAGATGGCGGCCGCCGAGGGACCGGTGGGCGACGCGGGAGTG\r";
 
+        $aActions = [
+            "Remove non coding characters" => "remove_non_coding",
+            "Reverse sequence" => "reverse",
+            "Complement sequence" => "complement",
+            "Reverse and Complement of sequence" => "reverse_and_complement",
+            "Display Double-stranded Sequence" => "display_both_strands",
+            "Convert to RNA" => "toRNA"
+        ];
 
         $builder->add(
             'seq',
@@ -52,7 +57,53 @@ class SequenceManipulationType extends AbstractType
                 'required' => true
             ]
         );
-
+        $builder->add(
+            'action',
+            ChoiceType::class,
+            [
+                'choices' => $aActions,
+                'attr' => [
+                    'class' => "custom-select d-block w-20",
+                ],
+                'multiple' => true
+            ]
+        );
+        $builder->add(
+            'start',
+            TextType::class,
+            [
+                'label' => "Select subsequence from position : ",
+                'attr' => [
+                    'class' => "form-control"
+                ]
+            ]
+        );
+        $builder->add(
+            'end',
+            TextType::class,
+            [
+                'label' => "to (both included)",
+                'attr' => [
+                    'class' => "form-control"
+                ]
+            ]
+        );
+        $builder->add(
+            'GC',
+            CheckboxType::class,
+            [
+                'required' => false,
+                'label' => "G + C content"
+            ]
+        );
+        $builder->add(
+            'ACGT',
+            CheckboxType::class,
+            [
+                'required' => false,
+                'label' => "Nucleotide composition"
+            ]
+        );
         $builder->add(
             'submit',
             SubmitType::class,
