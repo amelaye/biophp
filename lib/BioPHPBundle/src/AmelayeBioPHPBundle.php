a9337dd5937cb92ee1603da04b77f44f316d658c
Amélie DUVERNET <amelieonline@gmail.com>
2020-01-20 18:05:52 +0100

    Adding comments

--- lib/BioPHPBundle/src/AmelayeBioPHPBundle.php
+++ lib/BioPHPBundle/src/AmelayeBioPHPBundle.php
@@ -1,9 +1,18 @@
 <?php
-
+/**
+ * Bundle initialisation
+ * Created 19 january 2020
+ * Last modified 19 january 2020
+ */
 namespace Amelaye\BioPHP;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class AmelayeBioPHPBundle
+ * @package Amelaye\BioPHP
+ * @author Amélie DUVERNET aka Amelaye <amelieonline@gmail.com>
+ */
 class AmelayeBioPHPBundle extends Bundle
 {
 }
--- lib/BioPHPBundle/src/DependencyInjection/AmelayeBioPHPExtension.php
+++ lib/BioPHPBundle/src/DependencyInjection/AmelayeBioPHPExtension.php
@@ -1,6 +1,10 @@
 <?php
-
-
+/**
+ * Dependency injections for the bundle
+ * Freely inspired by BioPHP's project biophp.org
+ * Created 19 january 2020
+ * Last modified 19 january 2020
+ */
 namespace Amelaye\BioPHP\DependencyInjection;
 
 
@@ -9,6 +13,11 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
+/**
+ * Class AmelayeBioPHPExtension
+ * @package Amelaye\BioPHP\DependencyInjection
+ * @author Amélie DUVERNET aka Amelaye <amelieonline@gmail.com>
+ */
 class AmelayeBioPHPExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
